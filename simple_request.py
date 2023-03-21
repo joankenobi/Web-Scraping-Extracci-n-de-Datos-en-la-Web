c9d35fca4db409be09aa6cd78adba0dbadf9d027
joankenobi <joan1233blanco@gmail.com>
2023-03-21 07:00:22 -0400

    the notebook added come from the class

--- simple_request.py
+++ simple_request.py
@@ -1,5 +1,9 @@
 #%%
 import requests
+import lxml
+from bs4 import BeautifulSoup as bsoup
+from bs4.element import Tag
+
 
 url="https://www.pagina12.com.ar/"
 
@@ -33,3 +37,49 @@ p12.request.method
 #%%
 #es util ver la URL para ver si un sitio nos redirecciona a otro, y se hace de nuevo la solicitud de forma automatica.
 p12.request.url
+
+#%%
+#Convierte el requests a un tipo de objeto soup manejado por bs4
+# 'html.parser' deberia ser 'lxml' pero no ha funcionado
+soup=bsoup(p12.text, 'html.parser')
+
+#%%
+#mostramos el soup de forma ordenada como el inspector de los browsers
+print(soup.prettify())
+
+#%%
+#buscando en la sopa
+secciones=soup.find(
+    'ul', #trae el primer elemento del tipo ul
+    attrs={
+        "class":"horizontal-list main-sections hide-on-dropdown"
+        }#que coincida con el atributo
+    ).find_all(
+    "li"
+    )#dentro del 'ul' traer todos los 'li'
+type(secciones)
+
+#%%
+seccion:Tag=secciones[5]
+#obtener el link
+seccion.find("a"), seccion.a #ambos hacen lo mismo
+seccion.a.get("href")
+#obtener el texto
+seccion.text, seccion.a.get_text() #ambos hacen lo mismo
+
+#%%
+# Extraer todas las secciones y subsecciones
+links_seccion=[seccion.a.get("href") for seccion in secciones]
+sub_seccion_url=links_seccion[2]
+
+#extraer noticias de secciones(subsecciones)
+#crear sopa conr equest
+subseccion_text=requests.get(sub_seccion_url).text
+soup_sebseccion=bsoup(subseccion_text,"html.parser")
+
+articles=soup_sebseccion.find("div", 
+                     attrs={
+                        "class":"articles-list"
+                        }).find_all("div"
+                        )
+len(articles)
